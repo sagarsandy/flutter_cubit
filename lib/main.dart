d0c0813cc7e111d7523bf8258f98dc7828df5ab6
sagarsandy492 <sagar@invanto.com>
2021-03-05 13:21:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Cubit',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      appBar: AppBar(
+        title: Text("Cubit Flutter Bloc"),
+      ),
+      body: Container(
+        color: Colors.orange,
+        width: 200,
+        height: 200,
+      ),
+    );
+  }
+}
